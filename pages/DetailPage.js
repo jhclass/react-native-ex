2cd500326900eb6d93156ed0315a720f0521e8a2
jhclass <jhcode@naver.com>
2022-01-17 03:33:33 +0900

    Firebase : 데이터 쓰기(추가)

--- pages/DetailPage.js
+++ pages/DetailPage.js
@@ -1,7 +1,9 @@
 import React,{useState,useEffect} from 'react';
-import { StyleSheet, Text, View, Image, ScrollView,TouchableOpacity,Alert,Share } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView,TouchableOpacity,Alert,Share,Platform } from 'react-native';
 import * as Linking from 'expo-linking';
 import {firebase_db} from "../firebaseConfig"
+import * as Application from 'expo-application';
+const isIOS = Platform.OS === 'ios';
 
 export default function DetailPage({navigation,route}) {
 
@@ -32,8 +34,26 @@ export default function DetailPage({navigation,route}) {
         });
     },[])
 
-    const popup = () => {
-        Alert.alert("팝업!!")
+    const like = async () => {
+        
+        // like 방 안에
+        // 특정 사용자 방안에
+        // 특정 찜 데이터 아이디 방안에
+        // 특정 찜 데이터 몽땅 저장!
+        // 찜 데이터 방 > 사용자 방 > 어떤 찜인지 아이디
+        let userUniqueId;
+        if(isIOS){
+        let iosId = await Application.getIosIdForVendorAsync();
+            userUniqueId = iosId
+        }else{
+            userUniqueId = await Application.androidId
+        }
+
+        console.log(userUniqueId)
+	       firebase_db.ref('/like/'+userUniqueId+'/'+ tip.idx).set(tip,function(error){
+             console.log(error)
+             Alert.alert("찜 완료!")
+         });
     }
 
     const share = () => {
@@ -55,7 +75,7 @@ export default function DetailPage({navigation,route}) {
                 <Text style={styles.title}>{tip.title}</Text>
                 <Text style={styles.desc}>{tip.desc}</Text>
                 <View style={styles.buttonGroup}>
-                    <TouchableOpacity style={styles.button} onPress={()=>popup()}><Text style={styles.buttonText}>팁 찜하기</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={()=>like()}><Text style={styles.buttonText}>팁 찜하기</Text></TouchableOpacity>
                     <TouchableOpacity style={styles.button} onPress={()=>share()}><Text style={styles.buttonText}>팁 공유하기</Text></TouchableOpacity>
                     <TouchableOpacity style={styles.button} onPress={()=>link()}><Text style={styles.buttonText}>외부 링크</Text></TouchableOpacity>
                 </View>

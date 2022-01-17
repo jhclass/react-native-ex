e6efca9812467c389f689cf31453dee518a19695
jhclass <jhcode@naver.com>
2022-01-18 03:38:54 +0900

    선택한 데이터를 특정페이지(ex장바구니)에 보여주기 / 선택한 데이터를  삭제하기

--- Components/LikeCard.js
+++ Components/LikeCard.js
@@ -1,18 +1,59 @@
 import React from 'react';
-import {View, Image, Text, StyleSheet,TouchableOpacity} from 'react-native'
-
+import {Alert,View, Image, Text, StyleSheet,TouchableOpacity,Platform} from 'react-native'
+import {firebase_db} from "../firebaseConfig"
+const isIOS = Platform.OS === 'ios';
+import * as Application from 'expo-application';
 //MainPage로 부터 navigation 속성을 전달받아 Card 컴포넌트 안에서 사용
-export default function LikeCard({content,navigation}){
+export default function LikeCard({content,navigation,tip, setTip}){
+
+    const detail = () => {
+        navigation.navigate('DetailPage',{idx:content.idx})
+    }
+
+    const remove = async (cidx) => {
+      let userUniqueId;
+      if(isIOS){
+      let iosId = await Application.getIosIdForVendorAsync();
+          userUniqueId = iosId
+      }else{
+          userUniqueId = await Application.androidId
+      }
+
+      console.log(userUniqueId)
+      firebase_db.ref('/like/'+userUniqueId+'/'+cidx).remove().then(function(){
+        Alert.alert("삭제 완료");
+        //내가 찝 해제 버튼을 누른 카드 idx를 가지고
+        //찝페이지의 찜데이터를 조회해서
+        //찜해제를 원하는 카드를 제외한 새로운 찜 데이터(리스트 형태!)를 만든다
+        let result = tip.filter((data,i)=>{
+          return data.idx !== cidx
+        })
+        //이렇게 만들었으면!
+        //LikePage로 부터 넘겨 받은 tip(찜 상태 데이터)를
+        //filter 함수로 새롭게 만든 찜 데이터를 구성한다!
+        console.log(result)
+        setTip(result)
+
+      })
+      
+    }
+
     return(
         //카드 자체가 버튼역할로써 누르게되면 상세페이지로 넘어가게끔 TouchableOpacity를 사용
-        <TouchableOpacity style={styles.card} onPress={()=>{navigation.navigate('DetailPage',content)}}>
+        <View style={styles.card}>
             <Image style={styles.cardImage} source={{uri:content.image}}/>
             <View style={styles.cardText}>
                 <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
                 <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
                 <Text style={styles.cardDate}>{content.date}</Text>
+                
+                <View style={styles.buttonGroup}>
+                    <TouchableOpacity style={styles.button} onPress={()=>detail()}><Text style={styles.buttonText}>자세히보기</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={()=>remove(content.idx)}><Text style={styles.buttonText}>찜 해제</Text></TouchableOpacity>
+              
+                </View>
             </View>
-        </TouchableOpacity>
+        </View>
     )
 }
 
@@ -48,5 +89,22 @@ const styles = StyleSheet.create({
     cardDate: {
       fontSize:10,
       color:"#A6A6A6",
+    },
+    buttonGroup: {
+        flexDirection:"row",
+    },
+    button:{
+        width:90,
+        marginTop:20,
+        marginRight:10,
+        marginLeft:10,
+        padding:10,
+        borderWidth:1,
+        borderColor:'deeppink',
+        borderRadius:7
+    },
+    buttonText:{
+        color:'deeppink',
+        textAlign:'center'
     }
 });
--- pages/LikePage.js
+++ pages/LikePage.js
@@ -1,225 +1,61 @@
-import React,{useState,useEffect} from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView , StatusBar} from 'react-native';
-
-
-
-import data from '../data2.json';
-import Card from '../Components/Cards';
+import React,{useState, useEffect} from 'react';
+import {ScrollView, Text, StyleSheet,Platform} from 'react-native';
 import LikeCard from '../Components/LikeCard';
 import Loading from '../Components/Loading';
-//import { StatusBar } from 'expo-status-bar';
-const main = 'https://storage.googleapis.com/sparta-image.appspot.com/lecture/main.png'
-export default function MainPage({navigation,route}) {
-  //useState 사용법
-	//[state,setState] 에서 state는 이 컴포넌트에서 관리될 상태 데이터를 담고 있는 변수
-  //setState는 state를 변경시킬때 사용해야하는 함수
+import * as Application from 'expo-application';
+const isIOS = Platform.OS === 'ios';
+import {firebase_db} from "../firebaseConfig"
 
-  //모두 다 useState가 선물해줌
-  //useState()안에 전달되는 값은 state 초기값
-  const [state,setState] = useState([])
-  const [cateState,setCateState] = useState([])
-  const [tip, setTip] = useState([{
-    "idx":3,
-    "category":"재테크",
-    "title":"잠자는 내 돈을 찾아라",
-    "image": "https://storage.googleapis.com/sparta-image.appspot.com/lecture/money1.png",
-    "desc":"‘새는 돈’에는 미처 몰랐던 카드 포인트, 휴면예금이나 환급금도 포함됩니다. 확실히 파악하지 못한 잠자는 돈을 찾아보고 자투리 돈들을 모으는 것도 중요합니다. 케이블방송, 위성방송 서비스를 이용하면서 중복 납부한 요금, 셋톱박스 보증금 등 돌려받지 않은 돈이 있는지 확인 해보세요. 또, 카드 포인트 통합 조회 서비스를 이용해 여러 개의 카드 포인트가 모두 얼마인지 체크해두는 것이 좋습니다. 보험해약 환급금, 휴면 보험금이나 휴면 예금을 찾아보고 돌려받는 일도 요즘에는 어렵지 않습니다.",
-    "date":"2020.09.09"
-},
-{
-    "idx":4,
-    "category":"재테크",
-    "title":"할인행사, 한정할인판매 문구의 함정 탈출!",
-    "image": "https://storage.googleapis.com/sparta-image.appspot.com/lecture/money2.png",
-    "desc":"‘안 사면 100% 할인’이라는 말 들어보셨나요? 견물생심, 좋은 물건을 보면 사고 싶기 마련입니다. 특히 대대적인 ‘할인 행사’ 중인 대형 마트에 갔을 때는 말할 것도 없겠죠. 따라서 생필품을 살 때, 한꺼번에 사서 사용하는 것보다 필요할 때 조금씩 구매하는 편이 좋습니다. 장을 보면서 대형마트에 자주 가다 보면 지금 필요한 것뿐 아니라 앞으로 필요할 것까지 사게 되어 지출이 커지기 때문입니다. 특히 할인 품목을 보면 뜻하지 않은 소비를 하는 경우도 많아진다. 홈쇼핑, 대형마트 등의 ‘할인행사’, ‘한정할인판매’ 등의 문구를 조심하세요. ",
-    "date":"2020.09.09"
-}])
-console.log(setTip);
-	//하단의 return 문이 실행되어 화면이 그려진다음 실행되는 useEffect 함수
-  //내부에서 data.json으로 부터 가져온 데이터를 state 상태에 담고 있음
-  const [ready,setReady] = useState(true)
-  StatusBar.setBackgroundColor("white");
-  StatusBar.setBarStyle('dark-content');
-
-  useEffect(()=>{
-	   
-		//뒤의 1000 숫자는 1초를 뜻함
-    //1초 뒤에 실행되는 코드들이 담겨 있는 함수
+export default function LikePage({navigation,route}){
     
-        //헤더의 타이틀 변경
+    const [tip, setTip] = useState([])
+    const [ready,setReady] = useState(true)
+
+    useEffect(()=>{
         navigation.setOptions({
-          title:"꿀팁 찜"
-      })  
-        setState(data.tip)
-        setCateState(data.tip)
-        
-        setReady(false)
-        //
+            title:'꿀팁 찜'
+        })
+        getLike()
+    },[])
 
-        //
-       
-  },[])
-
-  const category = (cate) => {
-    if(cate == "전체보기"){
-        //전체보기면 원래 꿀팁 데이터를 담고 있는 상태값으로 다시 초기화
-        setCateState(state)
-    }else if(cate == "꿀팁 찜"){
-console.log('선택')
-setCateState(tip)
-    }else{
-        setCateState(state.filter((d)=>{
-            return d.category == cate
-        }))
-    }
-}
-
-  //data.json 데이터는 state에 담기므로 상태에서 꺼내옴
-  // let tip = state.tip;
-  let todayWeather = 10 + 17;
-  let todayCondition = "흐림"
-  const aboutInfo = {title:'소개페이지',desc:'소개페이지를 소개합니다.'}
-  //return 구문 밖에서는 슬래시 두개 방식으로 주석
-  return ready ? <Loading/> :  (
-    /*
-      return 구문 안에서는 {슬래시 + * 방식으로 주석
-    */
-// /onPress={()=>{navigation.navigate('DetailPage',content)}
-    <ScrollView style={styles.container}>
-      <StatusBar />
-      {/* <Text style={styles.title}>나만의 꿀팁</Text> */}
-			 <Text style={styles.weather}>오늘의 날씨: {todayWeather + '°C ' + todayCondition} </Text>
-       <TouchableOpacity style={styles.introBtn} onPress={()=>{navigation.navigate('AboutPage',aboutInfo)}}><Text style={styles.middleButtonText}>소개페이지</Text></TouchableOpacity>
-      <Image style={styles.mainImage} source={{uri:main}}/>
-      <ScrollView style={styles.middleContainer} horizontal indicatorStyle={"white"}>
-      <TouchableOpacity style={styles.middleButtonAll} onPress={()=>{category('전체보기')}}><Text style={styles.middleButtonTextAll}>전체보기</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton01} onPress={()=>{category('생활')}}><Text style={styles.middleButtonText}>생활</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton02} onPress={()=>{category('재테크')}}><Text style={styles.middleButtonText}>재테크</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton03} onPress={()=>{category('반려견')}}><Text style={styles.middleButtonText}>반려견</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton04} onPress={()=>{category('꿀팁 찜')}}><Text style={styles.middleButtonText}>꿀팁 찜</Text></TouchableOpacity>
-      </ScrollView>
-      <View style={styles.cardContainer}>
-         {/* 하나의 카드 영역을 나타내는 View */}
-         {
-          cateState.map((content,i)=>{
-            return (<Card content={content} key={i} navigation={navigation}/>)
-          })
+    const getLike = async () => {
+        let userUniqueId;
+        if(isIOS){
+        let iosId = await Application.getIosIdForVendorAsync();
+            userUniqueId = iosId
+        }else{
+            userUniqueId = await Application.androidId
         }
-        
-      </View>
-   
-    </ScrollView>)
+
+        console.log(userUniqueId)
+        firebase_db.ref('/like/'+userUniqueId).once('value').then((snapshot) => {
+            console.log("파이어베이스에서 데이터 가져왔습니다!!")
+            let tip = snapshot.val();
+            let tip_list = Object.values(tip)
+            if(tip_list && tip_list.length > 0){
+                setTip(tip_list)
+                setReady(false)
+            }
+            
+        })
+    }
+
+    return (
+        <ScrollView style={styles.container}>
+           {
+               tip.map((content,i)=>{
+                   // LikeCard에서 꿀팀 상태 데이터(==tip)과 꿀팁 상태 데이터를 변경하기 위한
+                   // 상태 변경 함수(== setTip)을 건네준다.
+                   //즉 자기 자신이 아닌, 자식 컴포넌트에서도 부모의 상태를 변경할 수 있다.
+                   return(<LikeCard key={i} content={content} navigation={navigation} tip={tip} setTip={setTip}/>)
+               })
+           }
+        </ScrollView>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    //앱의 배경 색
-    backgroundColor: '#fff',
-  },
-  title: {
-    //폰트 사이즈
-    fontSize: 20,
-    //폰트 두께
-    fontWeight: '700',
-    //위 공간으로 부터 이격
-    marginTop:50,
-    //왼쪽 공간으로 부터 이격
-    marginLeft:20
-  },
-weather:{
-    alignSelf:"flex-end",
-    paddingRight:20,
-    marginTop:20,
-  },
-  introBtn:{
-    alignSelf:"flex-end",
-    flex:1,
-    justifyContent:'center',
-    width:100,
-    height:50,
-    
-    backgroundColor:"#20b2aa",
-    borderColor:"deeppink",
-    borderRadius:5,
-    marginRight:20,
-    marginTop:20
-  },
-  mainImage: {
-    //컨텐츠의 넓이 값
-    width:'90%',
-    //컨텐츠의 높이 값
-    height:200,
-    //컨텐츠의 모서리 구부리기
-    borderRadius:10,
-    marginTop:20,
-    //컨텐츠 자체가 앱에서 어떤 곳에 위치시킬지 결정(정렬기능)
-    //각 속성의 값들은 공식문서에 고대로~ 나와 있음
-    alignSelf:"center"
-  },
-  middleContainer:{
-    marginTop:20,
-    marginLeft:10,
-    height:60
-  },
-  middleButtonAll: {
-    width:100,
-    height:50,
-    padding:15,
-    backgroundColor:"#20b2aa",
-    borderColor:"deeppink",
-    borderRadius:15,
-    margin:7
-  },
-  middleButton01: {
-    width:100,
-    height:50,
-    padding:15,
-    backgroundColor:"#fdc453",
-    borderColor:"deeppink",
-    borderRadius:15,
-    margin:7
-  },
-  middleButton02: {
-    width:100,
-    height:50,
-    padding:15,
-    backgroundColor:"#fe8d6f",
-    borderRadius:15,
-    margin:7
-  },
-  middleButton03: {
-    width:100,
-    height:50,
-    padding:15,
-    backgroundColor:"#9adbc5",
-    borderRadius:15,
-    margin:7
-  },
-  middleButton04: {
-    width:100,
-    height:50,
-    padding:15,
-    backgroundColor:"#f886a8",
-    borderRadius:15,
-    margin:7
-  },
-  middleButtonText: {
-    color:"#fff",
-    fontWeight:"700",
-    //텍스트의 현재 위치에서의 정렬 
-    textAlign:"center"
-    
-  },
-  middleButtonTextAll: {
-    color:"#fff",
-    fontWeight:"700",
-    //텍스트의 현재 위치에서의 정렬 
-    textAlign:"center"
-  },
-  cardContainer: {
-    marginTop:10,
-    marginLeft:10
-  },
-
-
-});
+    container:{
+        backgroundColor:"#fff"
+    }
+})
